49b2da213d3a858dd52e869550c163cd0296d6ce
Chandler Casey <chandlercasey13@gmail.com>
2024-07-09 14:48:53 -0500

    changed port under app.listen to variable

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const port = process.env.PORT ? process.env.PORT : "3000";
+const dotenv = require('dotenv');
+dotenv.config();
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-  });
-
- io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-    
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`The express app is ready on port ${port}!`);
 });
