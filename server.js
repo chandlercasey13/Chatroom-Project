e296fc544cef2d5727c54a94271a590796c505f7
Chandler Casey <chandlercasey13@gmail.com>
2024-07-09 22:28:19 -0500

    added socket io event listeners that will console.log any message that comes from front end

--- server.js
+++ server.js
@@ -3,28 +3,31 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const cors = require('cors')
+const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:5173"
+    }
+  });
+
 const port = process.env.PORT ? process.env.PORT : '3000';
 
 
+app.use(cors())
 
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-  });
-
+//to setup event listeners, io.on must be wrapping socket.on methods
  io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-    
+    console.log('a user connected');
+
+  socket.on('message', (arg) => {
+    console.log(arg)
   });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+
 });
 
 server.listen(port, () => {
